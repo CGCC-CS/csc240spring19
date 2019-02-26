b89e69f90121974ee74864560dffcbf4d7f9f63e
Wade Huber <wadehuber@gmail.com>
2019-02-25 23:23:34 -0500

    Week 5 changes

--- cpp2/destructors.cpp
+++ cpp2/destructors.cpp
@@ -2,86 +2,83 @@
 
 using namespace std;
 
-class myClass {
+class MyClass {
   public:
-    int *a;
+    int * a;
   private:
     int b;
+    string name;
 
   public:
     // Constructor
-    myClass(int x) {
+    MyClass(int x, string n="") {
       static int counter = 1;
-
       a = new int;
       *a = x;
       b = counter;
-      counter++;
-      cout << "  Constructor " << b << " : " << a << " (" << *a << ")" << endl;
+      counter ++;
+      name = n;
+      cout << "  Constructor " << name << " " << b << " : " << a << " (" << *a << ")" << endl;
     }
 
     // Copy constructor
-    myClass(const myClass& original) {
+    MyClass(const MyClass& original) {
       a = new int;
       *a = *(original.a);
       b = original.b * 10;
-      cout << "  Copy Constructor " << b << " : " << a << " (" << *a << ")" << endl;
+      name = "COPY";
+      cout << "    Copy Ctor " << name << " " << b << " : " << a << " (" << *a << ")" << endl;
     }
 
-    // Destructor
-    ~myClass() {
-      cout << "  Destructor " << b << " : " << a << " (" << *a << ")" << endl;
+    ~MyClass() {
+      cout << "   Destructor " << name << " " << b << " : " << a << " (" << *a << ")" << endl;
       delete a;
-      a = nullptr;  
+      a = nullptr;
     }
 
-    void print(string s) {
-      cout << "  object " << b << " : " << a << " (" << *a << ")" << endl;
+    void print() {
+      cout << "       object " << name << " " << b << " : " << a << " (" << *a << ")" << endl;
     }
 };
 
-void func(myClass k) {
-  cout << "FUNC: ++++++++++++ start func ++++++++++++" << endl;
-  cout << "FUNC: start" << endl;
+void func(MyClass k) {
+ cout << " FUNC: ++++++++++++ start func ++++++++++++" << endl;
 
-  cout << "FUNC: begin declare c3" << endl;
-  myClass c3(3);
-  cout << "FUNC: end declare c3" << endl;
+ cout << " FUNC: begin declare c3" << endl;
+ MyClass c3(3, "c3");
+ cout << " FUNC: end declare c3" << endl;
 
-  cout << "FUNC: print k" << endl;
-  k.print("k");
-  cout << "FUNC: end k" << endl;
+ cout << " FUNC: begin print k" << endl;
+ k.print();
+ cout << " FUNC: end print k" << endl;
 
-  cout << "FUNC: print c3" << endl;
-  c3.print("c3");
-  cout << "FUNC: end c3" << endl;
+ cout << " FUNC: begin print c3" << endl;
+ c3.print();
+ cout << " FUNC: end print c3" << endl;
 
-  cout << "FUNC: end" << endl;
-  cout << "FUNC: +++++++++++++ end func +++++++++++++" << endl;
+ cout << " FUNC: +++++++++++++ end func _++++++++++++" << endl;
 }
 
 int main() {
   cout << "MAIN: start" << endl;
   cout << "MAIN: begin declare c1" << endl;
-  myClass c1(1);
+  MyClass c1(1, "c1");         // MyClass object allocated on stack
   cout << "MAIN: end declare c1" << endl;
-  
   cout << "MAIN: begin declare c2" << endl;
-  myClass * c2;
+  MyClass * c2;
   cout << "MAIN: end declare c2" << endl;
-  
   cout << "MAIN: begin instantiate c2" << endl;
-  c2 = new myClass(2);
+  c2 = new MyClass(2, "c2");  // MyClass object allocated on heap
   cout << "MAIN: end instantiate c2" << endl;
- 
-  cout << "MAIN: begin call func()" << endl;
+
+  cout <<" MAIN: begin call func()" << endl;
   func(c1);
-  cout << "MAIN: end call func() c2" << endl;
- 
+  cout <<" MAIN: end call func()" << endl;
+
   cout << "MAIN: begin delete c2" << endl;
   delete c2;
   cout << "MAIN: end delete c2" << endl;
- 
+  
   cout << "MAIN: exiting" << endl;
   return 0;
 }
