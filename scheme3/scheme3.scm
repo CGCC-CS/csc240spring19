ebee9ebd8a24a26028c1c7ad10b7c9790521d63e
Wade Huber <wadehuber@gmail.com>
2019-04-02 00:03:45 -0400

    Scheme 3 changes

--- scheme3/scheme3.scm
+++ scheme3/scheme3.scm
@@ -30,70 +30,19 @@
     (cond
       ((>= n 900) 'A)
       ((>= n 800) 'B)
+      ((>= n 700) 'C)
+      ((>= n 600) 'D)
       (else 'F))))
 
 (grade 100)
 (grade 950)
-(grade 830)
-
-(define rev
-  (lambda (lst)
-    (if (null? lst)
-        '()
-        (append (rev (cdr lst)) (list (car lst))))))
-(rev '(1 2 3 4 5))
-
-(define len
-  (lambda (lst)
-    (if (or (not (list? lst)) (null? lst))
-        0
-        (+ 1 (len (cdr lst))))))
-
-(define crazylist '(10 hello world (1 2 3 4) "csc" "240" (x y z w)))
-(define crazierlist '(10 hello world (1 (2 3) 4) "csc" "240" (a (b c) (d) (e))))
-(len crazylist)
-
-; Returns a procedure based on the parameter
-(define doThis
-  (lambda (n)
-    (cond
-      ( (> n 10) abs)
-      ( (> n 5) (lambda (k) (* k 3)))
-      (else (lambda (k) (+ k n))))))
-(do (doThis 12) 4)
-(do (doThis 7) 8)
-(do (doThis 2) 3)
-(do (doThis 3) 4)
-
-;compound statements
-(define (do_stuff x)
-  (begin
-    (display x)(newline)
-    (display "x!=")
-    (display "")(abs x)(newline)
-    (display "x^2=")(* x x)
-    (display "the end")
-    ))
-
-; Count all elements in a list and its sub-lists
-(define element-count
-  (lambda (lst)
-    (cond
-      ((null? lst) 0)
-      ((not (pair? lst)) 1)
-      (else (+ (element-count (car lst)) (element-count (cdr lst)))))))
-(element-count crazylist)
-(element-count crazierlist)
-(element-count 1)
-(element-count '(a))
-(element-count '(1 . 2))
-(element-count '((1 . 2) . (3 4 5)))
+(grade 899)
 
 "Multiply-all"
 (define multiply-all
   (lambda (lst)
-    (if (= (len lst) 1)
-        (car lst)
+    (if (null? lst)
+        1
         (* (car lst) (multiply-all (cdr lst))))))
 (multiply-all '(1 2 3 4))
 
@@ -103,32 +52,34 @@
   (lambda (operator base-case lst)
     (if (null? lst)
         base-case
-        (operator (car lst) (red operator base-case(cdr lst))))))
+        (operator (car lst) (red operator base-case (cdr lst))))))
 
-(red * 1 '(1 2 3 4))
 (red + 0 '(1 2 3 4))
+(red * 1 '(1 2 3 4))
 
 (define sum-all
   (lambda (lst)
     (red + 0 lst)))
+(sum-all '(1 2 3 4))
 
 (define mult-all
   (lambda (lst)
     (red * 1 lst)))
+(mult-all '(1 2 3 4))
 
-(define lst0 '(5 6 7 8))
-(sum-all lst0)
-(mult-all lst0)
+(define double-lst
+  (lambda (lst)
+    (red (lambda (x y) (cons (* 2 x) y)) '() lst)))
+(double-lst '(1 2 3 4))
 
-;length
-(red (lambda (x y) (+ 1 y)) 0 lst0)
-;square-list
-(red (lambda (x y) (cons (* x x) y)) '() lst0)
-;get-odds
-(red (lambda (x y) (if (= 0 (remainder x 2)) y (cons x y))) '() lst0)
-;reverse
-(red (lambda (x y) (append y (list x))) '() lst0)
-
+"length"
+(red (lambda (x y) (+ 1 y)) 0 '(1 2 3 4))
+"square list"
+(red (lambda (x y) (cons (* x x) y)) '() '(1 2 3 4))
+"get-odds"
+(red (lambda (x y) (if (= 0 (remainder x 2)) y (cons x y))) '() '(1 2 3 4 5 6 7 8 9))
+"reverse"
+(red (lambda (x y) (append y (list x))) '() '(1 2 3 4))
 
 
 (define lst '(1 2 3 4 5 6 7 8 9 10 11 12 13))
@@ -136,8 +87,8 @@
 (define badlst (list "CGCC" 1 'a + 10 'hello lst '(1 2 3) '(a . b) red 8 6 -3))
 
 (newline)
-"Higher order functions - apply"
-(apply + lst)   ; (+ 1 2 3 4 5 6 7 8 9 10 11 12 13)
+"Higher-order functions - apply"
+(apply + lst)
 (apply + lstn)
 (apply * lst)
 (apply * lstn)
@@ -147,17 +98,17 @@
 (map (lambda (x) (* x x)) lst)
 (map (lambda (x) (* x x)) lstn)
 (map (lambda (x) (if (> x 0) x (* -1 x))) lstn)
+(map (lambda (x) (if (>= x 0) #t #f)) lst)
 (map (lambda (x) (if (>= x 0) #t #f)) lstn)
 (map string? badlst)
 
 (newline)
 "Higher-order functions - filter"
-(filter (lambda (x) (if (>= x 0) #t #f)) lstn)
 (filter string? badlst)
 (filter pair? badlst)
-(filter (lambda (x) (not (pair? x))) badlst)
-; sum the squares of the numbers in badlst
 (filter number? badlst)
+(filter (lambda (x) (if (>= x 0) #t #f)) lstn)
+(filter (lambda (x) (not (pair? x))) badlst)
 (map (lambda (x) (* x x)) (filter number? badlst))
 (apply + (map (lambda (x) (* x x)) (filter number? badlst)))
 
@@ -168,27 +119,36 @@
 (sum-num-square lst)
 (sum-num-square badlst)
 
-; numbers in badlst which < 8 when incremented
 badlst
-(filter (lambda(x) (< x 8))
+; number in badlst which are less 8 when incremented
+(filter (lambda (x) (< x 8))
         (map (lambda (x) (+ x 1))
              (filter number? badlst)))
-; correct - filter with increment
 (filter (lambda (x) (< (+ x 1) 8))
         (filter number? badlst))
 
-; Currying
+(apply - '(1 2 3 4))
+(apply + (map - '(1 2 3 4)))
+
+(newline)
+"Newline"
 (define big_num
   (lambda (n)
     (lambda (x)
       (> x n))))
 (big_num 10)
 (define positive? (big_num 0))
-(define winner? (big_num 10))
 (positive? 3)
-(positive? -10)
-(winner? 8)
-(winner? 100)
+(positive? -3)
+(define winner? (big_num 10))
+(winner? 3)
+(winner? 14)
+
+(define increase
+  (lambda (n)
+    (lambda (x)
+      (+ x n))))
+(map (increase 10) '(1 2 3 4))
 
 (newline)
 "Define a polynomial"
@@ -196,30 +156,35 @@ badlst
   (lambda (a b c)
     (lambda (x)
       (+ (* a x x) (* b x) c))))
-(define f1 (quad 1 1 1))   ;f(x) = x^2 + x + 1
+(quad 1 1 1)
+(define f1 (quad 1 1 1))   ; f(x) = x^2 + x + 1
 (define f2 (quad 1 2 3))   ; f(x) = x^2 + 2x + 3
 (define f3 (quad 2 4 2))   ; f(x) = 2x^2 + 4x + 2
-(define f4 (quad 1 (/ 1 2) (/ 1 4))) ; f(x) = x^2 + 1/2 x + 1/4
+(define f4 (quad 1 (/ 1 2) (/ 1 4)))   ; f(x) = x^2 + 1/2 x + 1/4
 f1
 (f1 2)
 (f1 -2)
 (f2 2)
+(f2 -2)
 (f3 2)
 (f4 2)
+
+(require "scheme3inc.scm")
+(fib-tail 10)
 
 (newline)
 "Towers of Hanoi"
 (define hanoi
   (lambda (n source center destination)
-    (if (= n 1)  ; stopping conditionkj
-      (list 'Move source destination)
-      (list (hanoi (- n 1) source destination center)
-            (hanoi 1 source center destination)
-            (hanoi (- n 1) center source destination)))))
+    (if (= n 1)
+        (list 'Move source destination)
+        (list (hanoi (- n 1) source destination center)
+              (hanoi 1 source center destination)
+              (hanoi (- n 1) center source destination)))))
 (hanoi 3 'red 'yellow 'green)
 
 (newline)
-"Let structures (scope)"
+"let structures (scope)"
 (define a 100)
 a
 
@@ -230,16 +195,16 @@ a
 
 (let
     ((a 4)
-     (b (+ a 1)))   ; a comes from the outer scope
+     (b (+ a 1)))  ; a comes from the outer scope
   (+ a b))
 
 (let
     ((a 4))
   (let
-      ((b (+ a 1))) ; a comes from the inner scope
+      ((b (+ a 1)))  ; a comes from the inner scope
     (+ a b)))
 
-; You can make things confusing
+; You can make thigns confusing
 (let
     ((x 9))
   (let
@@ -252,7 +217,7 @@ a
     (let
         ((a (* 2 x))
          (b (+ 1 x))
-         (c 10))
+         (c 5))
       (+ x a b c))))
 (let-test 10)
 
@@ -276,12 +241,14 @@ a
   (* + -))
 
 (newline)
-"lambda->let, let->lambda)"
+"lambda->let, let->lambda"
 (let
     ((a 4)
      (b 5))
   (+ a b))
+
 ((lambda (a b) (+ a b)) 4 5)
+
 
 ((lambda (x y) (+ (* x x) (* y y))) 3 4)
 (let
@@ -289,27 +256,24 @@ a
      (y 4))
   (+ (* x x) (* y y)))
 
-(require "scheme3inc.scm")
-(fib-tail 10)
-;(fib 10)
-
 (newline)
-"Advanced topics - not required for project or final"
+"Advanced topics - not required for projet or final"
+"case-lambda"
 (define hello
   (case-lambda
-    (() "Hello, World")
+    (() "Hello, world!")
     ((name) (string-append "Hello, " name))))
 (hello)
 (hello "John")
 
-(define multiple-of-2-and-5
+(define multiple-of-2-and-5?
   (lambda (n)
     (match (list (remainder n 2) (remainder n 5))
-      ((list 0 0) #t) ; both remainders are 0
+      ((list 0 0) #t)  ; both remainders are 0
       ((list _ 0) "Divisible only by 5")
       ((list 0 _) "Divisible only by 2")
       (_ #f))))
-(multiple-of-2-and-5 20)
-(multiple-of-2-and-5 15)
-(multiple-of-2-and-5 17)
-(multiple-of-2-and-5 8)
+(multiple-of-2-and-5? 20)
+(multiple-of-2-and-5? 15)
+(multiple-of-2-and-5? 17)
+(multiple-of-2-and-5? 8)
