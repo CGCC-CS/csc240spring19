61b3df7be46e747b1d8692a4015eee8effba2b5c
Wade Huber <wadehuber@gmail.com>
2019-04-09 00:18:54 -0400

    Prolog 1 changes

--- prolog1/prolog1.pl
+++ prolog1/prolog1.pl
@@ -9,13 +9,15 @@ mother(padme, leia).
 mother(leia, ben).
 alias(darthvader, anakin).
 alias(luke, skywalker).
-alias(X, Y) :- alias(Y, X).
+alias(X,Y) :- alias(Y, X).     % not a great rule - avoid circular logic!
 
+% male/1
 male(luke).
 male(john).
 male(bob).
 male(mike).
 female(sarah).
+% weather/3
 weather(tempe, summer, hot).
 weather(tempe, winter, warm).
 weather(tempe, fall, hot).
@@ -23,10 +25,12 @@ weather(newyork, summer, warm).
 weather(seattle, summer, cool).
 weather(buenasaires, winter, warm).
 weather(sydney, fall, hot).
-weather(City, Temp) :- weather(City, _, Temp).
+weather(yellowknife, fall, cold).
+% weather/2
+weather(City, Temp) :- weather(City, _Season, Temp).
+% warmcity/1
 warmcity(City) :- weather(City, _, warm).
 warmcity(City) :- weather(City, fall, hot).
-
 
 avg_temp(berlin, 49).
 avg_temp(karlsruhe, 60).
@@ -38,37 +42,37 @@ avg_temp(johannesburg, 55).
 avg_temp(phoenix, 80).
 avg_temp(jerusalem, 61).
 
-% calculate averate temperatur in Celsius
-avg_temp_celsius(City, C_temp) :-   % head of the rule
-		avg_temp(City, F_temp),     % get the avg temp of the City
-		C_temp is round((F_temp - 32) * 5 / 9).
+% calculate average temperature in Celsius
+avg_temp_celsius(City, C_temp) :-        % head of the rule
+			avg_temp(City, F_temp),   % get the avg temp of the City
+			C_temp is round((F_temp - 32) * 5 / 9).
 
 play_outside(City) :- avg_temp_celsius(City, Temp), Temp < 20.
 
 colder_than(City1, City2) :- 
-		avg_temp_celsius(City1, Temp1),
-		avg_temp_celsius(City2, Temp2),
-		Temp1 < Temp2.
+	            avg_temp(City1, Temp1),
+	            avg_temp(City2, Temp2),
+                Temp1 < Temp2.
 
 % To check equality, use the same variable name
 same_temp(City1, City2) :-
-		avg_temp_celsius(City1, Temp),
-		avg_temp_celsius(City2, Temp),
-		City1 \= City2.
+                avg_temp(City1, Temp),
+                avg_temp(City2, Temp),
+				City1 \= City2.
 
 president(washington).
 president(lincoln).
 president(kennedy).
 president(obama).
+president(captainmarvel).
 president(X) :- member(X,[adams, jefferson, fdr]).
 
-
-% Not a good rule
-score(suns, 102).
-score(grizzlies, 100).
-won(X) :- score(X, ScoreX), score(_, ScoreY), ScoreX > ScoreY.
-
-% how can we improve the above?
+score(phillies,4).
+score(nationals,3).
+score(rays, 5).
+score(whitesox, 1).
+% This is not a good rule!
+won(X) :- score(X, ScoreX), score(_Y, ScoreY), ScoreX > ScoreY.
 
 happy(bob).
 happy(alice).
@@ -85,20 +89,13 @@ badguy(batman, twoface).  % badguy/2
 badguy(batman, joker).
 badguy(superman, lexluthor).
 fight(X) :- badguy(X).
-fight(X) :- badguy(_, X).
-% poor bob & alice
-fight(X) :- annoying(X).
-fight(alice).
-fight(_).       % Nooooooooo!
+fight(X) :- badguy(_,X).
+fight(X) :- annoying(X).  % poor bob!
+fight(alice).             % poor alice!
+fight(_).                 % poor anybody!
 
-% write output when a file is consulted
-:- badguy(batman, Who), write('Batman has defeated '), write(Who), nl.
 
-same(X,X).
+num(X) :- member(X,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]).
 
 % equation A + B = C
-equation(A,B,C) :-
-	same(Integer,[0,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20]),
-	member(A, Integer),
-	member(B, Integer), 
-	C is A + B.
+equation(A,B,C) :- num(A), num(B), C is A + B.
